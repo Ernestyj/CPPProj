b37c54eecfea6892fdf05a811d5401605f795705
Eugene <ernestyj@outlook.com>
2019-12-11 21:18:31 +0800

    c

--- tests/unit_tests/test_mycpp.cpp
+++ tests/unit_tests/test_mycpp.cpp
@@ -1,15 +1,62 @@
 #include "gtest/gtest.h"
-
 #include <iostream>
 
 using namespace std;
 
 
+struct QuickSort{
+    void quickSort(vector<int>& nums){
+        quickSort(nums, 0, nums.size() - 1);
+    }
+    void quickSort(vector<int>& arr, int left, int right) {
+        int l = left, r = right, pivot = arr[(left+right)/2];
+        while (l<=r) {
+            while (arr[l]<pivot) l++;
+            while (arr[r]>pivot) r--;
+            if (l<=r) {
+                swap(arr[l], arr[r]);
+                l++; r--;
+            }
+        };// 最后退出的情况应该是右指针在左指针左边一格
+        if (left <= r) quickSort(arr, left, r);  //TODO 注意边界
+        if (l <= right) quickSort(arr, l, right);
+    }
+};
+
+struct ReverseWords{
+    static string reverseWords(string s) {
+        stringstream ss(s);
+        string token, res;
+        while(getline(ss, token, ' ')){
+            cout<<token<<"|";
+            if(token!="") res = token+" "+res;
+        }
+        cout<<res.size();
+        res.pop_back();
+        return res;
+    }
+};
+
+
 namespace {
     // Normal Test case
-    TEST(Example, E) {
-      EXPECT_EQ(1, 1);
-      cout<<"Test finished."<<endl;
+//    TEST(Example, E) {
+//      EXPECT_EQ(1, 1);
+//      cout<<"Test finished."<<endl;
+//    }
+
+//    TEST(QuickSort, 1) {
+//        QuickSort ins;
+//        vector<int> nums = {5,4,3,2,1,0,100};
+//        ins.quickSort(nums);
+//        for(auto a: nums){
+//            cout<<a<<" ";
+//        }
+//        cout<<endl;
+//    }
+
+    TEST(ReverseWords, 2){
+        ReverseWords::reverseWords("   ");
     }
 
     // The fixture for testing class Foo.
@@ -37,10 +84,9 @@ namespace {
         // Objects declared here can be used by all tests in the test case for Foo.
     };
 
-    // Tests that Foo does Xyz.
-    TEST_F(MyCppTest, DoesXyz) {
-      // Exercises the Xyz feature of Foo.
-    }
+//    TEST_F(MyCppTest, DoesXyz) {
+//      // Exercises the Xyz feature of Foo.
+//    }
 }
 
 //int main(int argc, char **argv) {
